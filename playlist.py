63db67b93daf7c78d8d108d236caad4f1c7c17ce
azazpaka <alexander.finley@ascte.org>
2026-09-22 08:27:07 -0500

    added the songs

---
+++ playlist.py
@@ -0,0 +1,8 @@
+playlist=[
+    {"song 1": "Dominated Love Slave", "Artist": "Green Day"},
+    {"song 2": "Evil Dog", "Artist": "Diversity Hires"},
+    {"song 3": "Walking Contradiction", "Artist": "Green Day"},
+    {"song 4": "Racing Glove", "Artist": "Plumtree"}
+]
+
+print(playlist)
